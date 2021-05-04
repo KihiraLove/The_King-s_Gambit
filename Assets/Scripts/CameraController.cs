e5702eb9e74fd77c59eafcd75fe3dce4b998781b
KihiraLove <KihiraLove@github.com>
2021-05-04 12:50:49 +0200

    New Cam Script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,67 +1,45 @@
 ﻿using UnityEngine;
 
-[AddComponentMenu("Camera Control")]
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
-    public float distance = 10.0f;
-    public float xSpeed = 100.0f;
-    public float ySpeed = 100.0f;
+    [SerializeField] private float lookSpeedH = 1.6f;
 
-    public float yMinLimit = -20f;
-    public float yMaxLimit = 80f;
+    [SerializeField] private float lookSpeedV = 1.6f;
 
-    public float distanceMin = 1f;
-    public float distanceMax = 20f;
+    [SerializeField] private float zoomSpeed = 6f;
 
-    public Texture2D _cursorTexture;
-    private readonly CursorMode _cursorMode = CursorMode.Auto;
+    [SerializeField] private float dragSpeed = 32f;
 
-    private float _x;
-    private float _y;
+    private float _yaw;
+    private float _pitch;
 
     private void Start()
     {
-        var angles = transform.eulerAngles;
-        _x = angles.y;
-        _y = angles.x;
+        // Initialize the correct initial rotation
+        var eulerAngles = transform.eulerAngles;
+        _yaw = eulerAngles.y;
+        _pitch = eulerAngles.x;
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(0) && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.LeftShift))
+        //Look around with Right Mouse
+        if (Input.GetMouseButton(1))
         {
-            _x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
-            _y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
-            _y = ClampAngle(_y, yMinLimit, yMaxLimit);
-            Cursor.SetCursor(_cursorTexture, Vector2.zero, _cursorMode);
-        }
-        else
-        {
-            Cursor.SetCursor(null, Vector2.zero, _cursorMode);
+            _yaw += lookSpeedH * Input.GetAxis("Mouse X");
+            _pitch -= lookSpeedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(_pitch, _yaw, 0f);
         }
 
-        var rotation = Quaternion.Euler(_y, _x, 0);
-        distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+        //drag camera around with Middle Mouse
+        if (Input.GetMouseButton(2))
+        {
+            transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed,
+                -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
+        }
 
-        RaycastHit hit;
-
-        var _layerMask = 1 << 9;
-        if (Physics.Linecast(target.position, transform.position, out hit, _layerMask)) distance -= hit.distance;
-
-        var negDistance = new Vector3(0.0f, 0.0f, -distance);
-        var position = rotation * negDistance + target.position;
-
-        transform.rotation = rotation;
-        transform.position = position;
-    }
-
-    private static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360F)
-            angle += 360F;
-        if (angle > 360F)
-            angle -= 360F;
-        return Mathf.Clamp(angle, min, max);
+        //Zoom in and out with Mouse Wheel
+        transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * this.zoomSpeed, Space.Self);
     }
 }

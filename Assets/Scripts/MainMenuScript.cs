05605d868a33d0317b101a945075de986085cb55
simonblnt <simon.blnt93@gmail.com>
2021-05-19 12:01:10 +0200

    Finished main menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public GameObject MainMenu;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        MainMenuButton();
+
     }
 
     public void PlayButton()
@@ -17,12 +17,6 @@ public class MainMenuScript : MonoBehaviour
         Debug.Log("Play Clicked");
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
-    }
-
-    public void MainMenuButton()
-    {
-        // Show Main Menu
-        MainMenu.SetActive(true);
     }
 
     public void QuitButton()

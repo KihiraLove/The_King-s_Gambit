b6e4f401e41f7ea892befef53af574e58e70fcb8
KihiraLove <illuminatecmdc@gmail.com>
2021-05-19 14:41:11 +0200

    last minute fixes

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,17 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     public void PlayButton()
     {
         Debug.Log("Play Clicked");
